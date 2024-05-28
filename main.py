e267490b352e9df95caccad2581e8132d0cfa437
djshaioh <dmsqlder@gmail.com>
2024-05-29 01:24:39 +0900

    Committing changes before rebasing

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import uvicorn
 
 app = FastAPI()
 
-origins = ["http://127.0.0.1:5500", "http://50.19.226.105 "]
+origins = ["http://127.0.0.1:5500", "http://3.224.62.21"]
 
 app.add_middleware(
 CORSMiddleware,
